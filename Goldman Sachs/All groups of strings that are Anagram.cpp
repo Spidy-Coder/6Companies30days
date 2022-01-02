558943df43db625a07aa6d9648f53d1aa73368e9
Paridhi Jain <18ec.paridhijain@gmail.com>
2022-01-02 20:33:57 +0530

    Update All groups of strings that are Anagram.cpp

--- Goldman Sachs/All groups of strings that are Anagram.cpp
+++ Goldman Sachs/All groups of strings that are Anagram.cpp
@@ -1,1 +1,26 @@
+// PROBLEM 1:- Given an array of strings, all groups of strings that are anagram
 
+//GFG SOLUTION
+
+//Accepted
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        
+        vector<vector<string>>ans;
+        unordered_map<string, vector<string>> map;
+        
+        for(auto str:string_list){
+            string word = str;
+            sort(word.begin(),word.end());
+            map[word].push_back(str);
+        }
+        
+        for(auto it:map){
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
